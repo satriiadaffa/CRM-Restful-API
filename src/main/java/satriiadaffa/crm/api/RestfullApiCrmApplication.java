2239081cb9dc398d109fd9fcd89554ed4aac25c9
satriiadaffa <satriiadaffa@gmail.com>
2025-02-21 23:06:52 +0700

    Initial Commit

---
+++ src/main/java/satriiadaffa/crm/api/RestfullApiCrmApplication.java
@@ -0,0 +1,13 @@
+package satriiadaffa.crm.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullApiCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullApiCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/satriiadaffa/crm/api/RestfullApiCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package satriiadaffa.crm.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullApiCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
